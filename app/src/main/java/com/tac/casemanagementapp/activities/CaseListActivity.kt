d4065a4663c8a1e6c871cb0148546c729412098d
Jacob <jjacobdofficial@gmail.com>
2025-10-23 22:07:43 +0100

    --> Added Case List UI components (menu_main, activity_case_list, card_case) for RecyclerView integration

--- app/src/main/java/com/tac/casemanagementapp/activities/CaseListActivity.kt
+++ app/src/main/java/com/tac/casemanagementapp/activities/CaseListActivity.kt
@@ -3,10 +3,15 @@ package com.tac.casemanagementapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tac.casemanagementapp.R
+import timber.log.Timber
 
 class CaseListActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_case_list)
+
+        Timber.plant(Timber.DebugTree())
+        Timber.i("CaseListActivity started")
     }
 }
